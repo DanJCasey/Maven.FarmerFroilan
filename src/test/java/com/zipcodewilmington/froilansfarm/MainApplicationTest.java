aad0fbcb981c8ca10d4a9462838c1da90a71db5f
Prathibha <prathibha@Zipcoders-MacBook-Pro-8.local>
2022-11-23 14:37:30 -0500

    added junit dependency

--- src/test/java/com/zipcodewilmington/froilansfarm/MainApplicationTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/MainApplicationTest.java
@@ -1,7 +1,17 @@
 package com.zipcodewilmington.froilansfarm;
 
+//import org.testng.annotations.Test;
+
+//import org.testng.annotations.Test;
+
+import org.junit.Test;
+
 /**
  * Created by leon on 2/26/18.
  */
 public class MainApplicationTest {
+    @Test
+    public void test(){
+
+    }
 }
