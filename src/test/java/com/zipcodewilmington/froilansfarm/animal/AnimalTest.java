7ae67cf24e6d8868e19317d62913ed48be1145d7
Collin <collin@Zipcoders-MacBook-Pro.local>
2022-11-27 14:11:27 -0500

    still working through

---
+++ src/test/java/com/zipcodewilmington/froilansfarm/animal/AnimalTest.java
@@ -0,0 +1,30 @@
+package com.zipcodewilmington.froilansfarm.animal;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AnimalTest {
+
+    public Animal animal;
+
+    public void setUp(){
+        animal = new Animal() {
+        };
+    }
+
+//    @Test
+//    public void getNameTest() {
+//        animal.setName("Bob");
+//        String expected = "Bob";
+//        String actual = animal.getName();
+//        Assert.assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void getAgeTest() {
+//        animal.setAge(17);
+//        int expected = 17;
+//        int actual = animal.getAge();
+//        Assert.assertEquals(expected, actual);
+//    }
+}
