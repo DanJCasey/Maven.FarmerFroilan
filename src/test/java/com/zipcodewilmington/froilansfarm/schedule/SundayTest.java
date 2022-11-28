cf6a55c7f5d65cad5b5a68817b169e7c58c9a03d
Prathibha <prathibha@Zipcoders-MacBook-Pro-8.local>
2022-11-27 19:00:50 -0500

    Addeds testmethod in Sunday TestClass

--- src/test/java/com/zipcodewilmington/froilansfarm/schedule/SundayTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/schedule/SundayTest.java
@@ -3,11 +3,16 @@ package com.zipcodewilmington.froilansfarm.schedule;
 import com.zipcodewilmington.froilansfarm.animal.Farmer;
 import com.zipcodewilmington.froilansfarm.animal.Pilot;
 import com.zipcodewilmington.froilansfarm.farmVehicle.CropDuster;
+import com.zipcodewilmington.froilansfarm.field.CropRow;
+import com.zipcodewilmington.froilansfarm.field.Field;
 import com.zipcodewilmington.froilansfarm.produce.Corn;
 import com.zipcodewilmington.froilansfarm.produce.Egg;
 import com.zipcodewilmington.froilansfarm.produce.Tomato;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SundayTest {
     @Test
@@ -57,5 +62,23 @@ public class SundayTest {
         Boolean actual = cduster.fly();
         //then
         Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void plantThreeCrops(){
+        //Given
+        Field objField=new Field();
+        List<CropRow> lstCropRowsExpected=new ArrayList<>();
+        CropRow expectedFirstCrop=new CropRow(objField.createTomatoCrop());
+        CropRow expectedSecondCrop=new CropRow(objField.createCornStalkCrop());
+        CropRow expectedThirdCrop=new CropRow(objField.createTomatoCrop());
+        //When
+        lstCropRowsExpected.add(expectedFirstCrop);
+        lstCropRowsExpected.add(expectedSecondCrop);
+        lstCropRowsExpected.add(expectedThirdCrop);
+        //Then
+        Assert.assertEquals(expectedFirstCrop,lstCropRowsExpected.get(0));
+        Assert.assertEquals(expectedSecondCrop,lstCropRowsExpected.get(1));
+        Assert.assertEquals(expectedThirdCrop,lstCropRowsExpected.get(2));
+
     }
 }
